500a72591d606393dd422c212a364a6d4174ae46
Rutger Schoorstra <schoorstra_rutger@hotmail.com>
2015-10-04 12:44:06 +0200

    Uses try_clone() on the TcpStream, so the server can open both a read and write buffer. This makes the server function properly now with only 1 socket needed (instead of using 2 sockets).

--- crossroad_server/src/main.rs
+++ crossroad_server/src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_json;
 extern crate schedule_recv;
 extern crate crossroad_server; // Local crate
 
+
 use serde::ser;
 use schedule_recv as sched;
 
@@ -29,52 +30,50 @@ fn run_server<A>(address: A) -> io::Result<()> where A: ToSocketAddrs + Display 
     let listener = try!(TcpListener::bind(&address));
     println!("Server listening on: {}", address);
 
-    // infinite loop
+    // Infinite loop.
     for tcp_stream in listener.incoming().filter_map(|i| i.ok()) {     
         thread::spawn(move || {
             println!("Connecting a new client");
-            handle_client(tcp_stream).unwrap();
+            
+            match handle_client(tcp_stream) {
+                Ok(_) => println!("Client disconnected"),
+                Err(v) => println!("Client Handle Error {:?}", v),
+            };
         });
     }
 
     Ok(())
 }
 
-fn connect_to_client(client_stream: &TcpStream) -> io::Result<TcpStream> {
-    client_stream.peer_addr().and_then(|ip| TcpStream::connect(ip))
-}
+fn handle_client(client_stream: TcpStream) -> io::Result<()> {
 
-fn handle_client(client_in_stream: TcpStream) -> io::Result<()> {
-
-    // Set up the 2-way connection
-    let client_out_stream = try!(connect_to_client(&client_in_stream));
-
-    // Convert streams to buffered streams
-    let client_out_writer = BufWriter::new(client_out_stream);
-    let client_in_reader = BufReader::new(client_in_stream); 
+    // Convert stream to buffered streams
+    let client_writer = BufWriter::new(try!(client_stream.try_clone()));
+    let client_reader = BufReader::new(client_stream); 
 
     // Main thread uses this channel to send (json) updates to the client.
     let (out_transmitter, out_receiver) = channel::<String>();
 
-    // Getting updates from the simulator(client) via the network, so make it safe with reference counter + a mutex.
+    // Getting updates from the simulator(client) via a socket, so make it safe with reference counter + a mutex.
     let client_baan_sensor_states = Arc::new(Mutex::new(BaanSensorStates::new()));
 
     // Run seperate threads
-    let client_receiver_handler = spawn_client_sensor_receiver(client_in_reader, client_baan_sensor_states.clone());
-    let client_updater_handler = spawn_client_updater(client_out_writer, out_receiver);
-    let verkeersregelinstallatie_handler = spawn_main_loop(out_transmitter, client_baan_sensor_states.clone());
+    let client_receiver_handle = spawn_client_sensor_receiver(client_reader, client_baan_sensor_states.clone());
+    let client_updater_handle = spawn_client_updater(client_writer, out_receiver);
+    let verkeersregelinstallatie_handle = spawn_main_loop(out_transmitter, client_baan_sensor_states.clone());
+
+    println!("Connection established");
 
     // Join all threads together
-    client_receiver_handler.join().unwrap();
-    client_updater_handler.join().unwrap();
-    verkeersregelinstallatie_handler.join().unwrap();
+    let joins = client_updater_handle.join()
+        .and_then(|_| client_receiver_handle.join())
+        .and_then(|_| verkeersregelinstallatie_handle.join());
 
     Ok(())
 }
 
 fn spawn_main_loop(tx: Sender<String>, sensor_shared_state: Arc<Mutex<BaanSensorStates>>) -> JoinHandle<Result<()>> {
     thread::spawn(move || {
-
         let frequency_scheduler = sched::periodic_ms(1000);
         loop {       
             frequency_scheduler.recv().unwrap();
@@ -82,8 +81,9 @@ fn spawn_main_loop(tx: Sender<String>, sensor_shared_state: Arc<Mutex<BaanSensor
             let ref mut sensor_state = *sensor_shared_state.lock().unwrap();
 
             // TODO: Traffic logic
-
             // TODO: send client new state
+
+            tx.send("New stoplicht state".to_string());
         }
     })
 }
@@ -93,8 +93,8 @@ fn spawn_client_sensor_receiver(mut reader: BufReader<TcpStream>, sensor_data: A
         let mut line = String::new();
         loop {  
             try!(reader.read_line(&mut line));
-            let ref mut traffic_state = *sensor_data.lock().unwrap();            
-            let baan = try!(traffic_state.update_from_json(&line)); 
+            //let ref mut traffic_state = *sensor_data.lock().unwrap();            
+            //let baan = try!(traffic_state.update_from_json(&line)); 
             println!("Client->Server: received baan sensor update: {:?}", line);     
         }
     })
@@ -111,20 +111,12 @@ fn spawn_client_updater(mut writer: BufWriter<TcpStream>, rx: Receiver<String>) 
     })   
 }
 
-
 #[test]
 fn verkeer() {
 /*
 
         let west = XorStoplichtGroep {
             naam: "Richting-West".to_string(),
-            stoplichten: vec![ 
-                StoplichtConflicting {
-                    stoplicht: &stoplichten[0], 
-                    conflicting_with: vec![ &stoplichten[1], &stoplichten[2] ]
-                },
-            ]
         };
-
 */
 }
