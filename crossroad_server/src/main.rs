047fa9897fdfbf60fa4b3c461491d804dbf3ceac
Rutger Schoorstra <schoorstra_rutger@hotmail.com>
2015-10-04 20:06:51 +0200

    Main loop now stops when client disconnects

--- crossroad_server/src/main.rs
+++ crossroad_server/src/main.rs
@@ -22,7 +22,7 @@ use crossroad_server::error::{Result, Error, JsonError};
 
 
 fn main() {
-    run_server("127.0.0.1:80").unwrap();    
+    run_server("127.0.0.1:9990").unwrap();    
 }
 
 fn run_server<A>(address: A) -> io::Result<()> where A: ToSocketAddrs + Display {
@@ -30,14 +30,14 @@ fn run_server<A>(address: A) -> io::Result<()> where A: ToSocketAddrs + Display 
     let listener = try!(TcpListener::bind(&address));
     println!("Server listening on: {}", address);
 
-    // Infinite loop.
+    // Infinite loop. 
     for tcp_stream in listener.incoming().filter_map(|i| i.ok()) {     
         thread::spawn(move || {
             println!("Connecting a new client");
             
             match handle_client(tcp_stream) {
-                Ok(_) => println!("Client disconnected"),
-                Err(v) => println!("Client Handle Error {:?}", v),
+                Ok(_) => println!("Client disconnected normally."),
+                Err(v) => println!("Client error {:?}", v),
             };
         });
     }
@@ -48,11 +48,12 @@ fn run_server<A>(address: A) -> io::Result<()> where A: ToSocketAddrs + Display 
 fn handle_client(client_stream: TcpStream) -> io::Result<()> {
 
     // Convert stream to buffered streams
-    let client_writer = BufWriter::new(try!(client_stream.try_clone()));
-    let client_reader = BufReader::new(client_stream); 
-
+    let client_reader = BufReader::new(try!(client_stream.try_clone()));
+    let client_writer = BufWriter::new(client_stream);
+    
     // Main thread uses this channel to send (json) updates to the client.
     let (out_transmitter, out_receiver) = channel::<String>();
+    let (exit_main_loop_tx, exit_main_loop_rx) = channel();
 
     // Getting updates from the simulator(client) via a socket, so make it safe with reference counter + a mutex.
     let client_baan_sensor_states = Arc::new(Mutex::new(BaanSensorStates::new()));
@@ -60,52 +61,68 @@ fn handle_client(client_stream: TcpStream) -> io::Result<()> {
     // Run seperate threads
     let client_receiver_handle = spawn_client_sensor_receiver(client_reader, client_baan_sensor_states.clone());
     let client_updater_handle = spawn_client_updater(client_writer, out_receiver);
-    let verkeersregelinstallatie_handle = spawn_main_loop(out_transmitter, client_baan_sensor_states.clone());
+    let verkeersregelinstallatie_handle = spawn_main_loop(out_transmitter, exit_main_loop_rx, client_baan_sensor_states.clone());
 
     println!("Connection established");
 
-    // Join all threads together
-    let joins = client_updater_handle.join()
-        .and_then(|_| client_receiver_handle.join())
-        .and_then(|_| verkeersregelinstallatie_handle.join());
+    // Wait for threads to exit
+    if let Err(v) = client_updater_handle.join().and(client_receiver_handle.join()) {
+        println!("client disconnected, error {:?}", v);
+    }
 
+    exit_main_loop_tx.send(0);
+    verkeersregelinstallatie_handle.join();
     Ok(())
 }
 
-fn spawn_main_loop(tx: Sender<String>, sensor_shared_state: Arc<Mutex<BaanSensorStates>>) -> JoinHandle<Result<()>> {
+fn spawn_main_loop( out_tx: Sender<String>, 
+                    exit_rx: Receiver<u8>, 
+                    sensor_shared_state: Arc<Mutex<BaanSensorStates>>)
+                    -> JoinHandle<Result<()>> 
+ {
     thread::spawn(move || {
         let frequency_scheduler = sched::periodic_ms(1000);
         loop {       
             frequency_scheduler.recv().unwrap();
 
+            if let Ok(exit_loop) = exit_rx.try_recv() {
+                break;
+            }
+            
             let ref mut sensor_state = *sensor_shared_state.lock().unwrap();
-
+            
             // TODO: Traffic logic
             // TODO: send client new state
-
-            tx.send("New stoplicht state".to_string());
+            
+            out_tx.send("New stoplicht state".to_string());
         }
-    })
+
+        Ok(())
+    })   
 }
 
 fn spawn_client_sensor_receiver(mut reader: BufReader<TcpStream>, sensor_data: Arc<Mutex<BaanSensorStates>>) -> JoinHandle<Result<()>> {  
-    thread::spawn(move || {
-        let mut line = String::new();
+    thread::spawn(move || {  
         loop {  
+            let mut line = String::new();
             try!(reader.read_line(&mut line));
-            //let ref mut traffic_state = *sensor_data.lock().unwrap();            
-            //let baan = try!(traffic_state.update_from_json(&line)); 
-            println!("Client->Server: received baan sensor update: {:?}", line);     
+
+            let ref mut traffic_state = *sensor_data.lock().unwrap();               
+
+            match traffic_state.update_from_json(&line) {
+                Ok(baan) => println!("Client->Server: received baan sensor update: {:?}", baan);
+                Err(err) => println!("Client->Server: received faulty json string {:?}\n{:?}", line, err);
+            }  
         }
     })
 }
 
-fn spawn_client_updater(mut writer: BufWriter<TcpStream>, rx: Receiver<String>) -> thread::JoinHandle<()> {
+fn spawn_client_updater(mut writer: BufWriter<TcpStream>, rx: Receiver<String>) -> thread::JoinHandle<Result<()>> {
     thread::spawn(move || {
         loop {
             let msg = rx.recv().unwrap();
-            writer.write(&msg.as_bytes()).unwrap();
-            writer.flush().unwrap();
+            try!(writer.write(&msg.as_bytes()));
+            try!(writer.flush());
             println!("Server->Client: sent new stoplicht state {:?}", msg);
         }
     })   
